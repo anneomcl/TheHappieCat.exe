f207fb782f99c7c46061929141bdda2933f59cdc
Anne <anneomclaughlin@gmail.com>
2017-06-13 20:36:43 -0700

    Initial commit
    Configuring VS Code for Python 3

---
+++ game.py
@@ -0,0 +1,11 @@
+
+def main():
+    """Starts the game"""
+    game()
+    print("I'm a monkey, this is a test")
+
+def game():
+    """The main game loop"""
+    print("Go through each block of dialogue and put it on the cmd terminal.")
+
+main()
